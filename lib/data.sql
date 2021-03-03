0f2cbc18d8093f66d4cc7f4506465c7521adb5e1
Yuri Nguyen <yuringuyen312@gmail.com>
2021-03-03 17:34:30 -0500

    DONE

--- lib/data.sql
+++ lib/data.sql
@@ -3,6 +3,7 @@ INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17), (2, 'Bear', 6), (3
 (11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
 (17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
 
+
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 (1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
 (2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
